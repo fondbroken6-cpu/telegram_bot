b3947a75868aca617a4c6f6e451f407905b9b6dc
fondbroken6-cpu <fondbroken6@gmail.com>
2026-05-13 09:56:10 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from aiogram import Bot, Dispatcher, types
 import asyncio
 import os
 
-BOT_TOKEN = os.getenv("8725166562:AAEF5MWJ7Wh2QjkVgEBEu07OrZ-2rug2auY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.channel_post()
-async def get_chat_id(message: types.Message):
+async def get_channel_id(message: types.Message):
     print("CHANNEL:", message.chat.title)
     print("ID:", message.chat.id)
     print("------")
