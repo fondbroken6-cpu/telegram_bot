09b94d892ae570d49523bce6e7cf4a6a5c430b11
fondbroken6-cpu <fondbroken6@gmail.com>
2026-05-13 08:52:36 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,20 @@
 from aiogram import Bot, Dispatcher, types
 import asyncio
+import os
 
-BOT_TOKEN = "8725166562:AAEF5MWJ7Wh2QjkVgEBEu07OrZ-2rug2auY"
-SOURCE_CHANNEL = -1003343428375
-
-TARGET_CHANNELS = {
-    "@testkanal1052": "https://t.me/testkanal1052"
-}
+BOT_TOKEN = os.getenv("8725166562:AAEF5MWJ7Wh2QjkVgEBEu07OrZ-2rug2auY")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.channel_post()
-async def autopost(message: types.Message):
-    if message.chat.id != SOURCE_CHANNEL:
-        return
-
-    text = message.caption or message.text or ""
-
-    for channel, link in TARGET_CHANNELS.items():
-        final_text = f"{text}\n\n👉 Kanalga o'tish:\n{link}"
-
-        if message.text:
-            await bot.send_message(chat_id=channel, text=final_text)
-
-        elif message.photo:
-            await bot.send_photo(
-                chat_id=channel,
-                photo=message.photo[-1].file_id,
-                caption=final_text
-            )
-
-        elif message.video:
-            await bot.send_video(
-                chat_id=channel,
-                video=message.video.file_id,
-                caption=final_text
-            )
+async def get_chat_id(message: types.Message):
+    print("CHANNEL:", message.chat.title)
+    print("ID:", message.chat.id)
+    print("------")
 
 async def main():
     await dp.start_polling(bot)
 
-asyncio.run(main())
+if name == "__main__":
+    asyncio.run(main())
